e8368a6a70a064ec5bba2f458a767242b13ea983
Ing.Samuel <126287892+SameuelxD@users.noreply.github.com>
2023-09-08 12:48:07 +0000

    en la lucha

--- Program.cs
+++ Program.cs
@@ -1,65 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
-using Estudiantes.Estudiante; // Asegúrate de importar el espacio de nombres correcto
 
-internal class Program
+namespace addEstudiantes
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        List<Entities> listClass = new List<Entities>();
-
-        for (int i = 0; i < 2; i++)
+        private static void Main(string[] args)
         {
-            Entities estudiante = new Entities();
-            Console.WriteLine("Ingrese Codigo Numerico");
+            List<Estudiante> listEstudiantes = new List<Estudiante>();
+            bool active = true;
+
+            while (active)
+            {
+                Console.WriteLine("-------------------- MENU --------------------");
+                Console.WriteLine("   1. Agregar Estudiante. ");
+                Console.WriteLine("   2. Agregar Notas. ");
+                Console.WriteLine("   3. Mostrar Estudiantes. ");
+                Console.WriteLine("   4. Cerrar Programa. ");
+                Console.WriteLine("----------------------------------------------");
+
+                if (int.TryParse(Console.ReadLine(), out int opcion))
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("1. Agregar Estudiante");
+                            Estudiante estudiante = AgregarEstudiante();
+                            listEstudiantes.Add(estudiante);
+                            break;
+                        case 2:
+                            Console.WriteLine("2. Agregar Notas");
+                            break;
+                        case 3:
+                            Console.WriteLine("3. Mostrar Estudiantes");
+                            MostrarEstudiantes(listEstudiantes);
+                            break;
+                        case 4:
+                            Console.WriteLine("Cerrando Programa. Presione una tecla para continuar.");
+                            Console.ReadLine();
+                            active = false;
+                            break;
+                        default:
+                            Console.WriteLine("Opción no válida. Intente de nuevo.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrada no válida. Intente de nuevo.");
+                }
+            }
+        }
+
+        static Estudiante AgregarEstudiante()
+        {
+            Estudiante estudiante = new Estudiante();
+
+            Console.WriteLine("Ingrese Código Numérico (máximo 15 caracteres):");
             estudiante.Codigo = Console.ReadLine();
-            while (estudiante.Codigo.Length > 15 || !int.TryParse(estudiante.Codigo, out _)){
-                Console.WriteLine("¡Error! Codigo Invalido");
-                Console.WriteLine("Ingrese Codigo Numerico");
+
+            while (estudiante.Codigo.Length > 15 || !int.TryParse(estudiante.Codigo, out _))
+            {
+                Console.WriteLine("¡Error! Código Inválido. Ingrese Código Numérico (máximo 15 caracteres):");
                 estudiante.Codigo = Console.ReadLine();
             }
 
-            Console.WriteLine("Ingrese Nombre");
+            Console.WriteLine("Ingrese Nombre (máximo 40 caracteres):");
             estudiante.Nombre = Console.ReadLine();
-            while(estudiante.Nombre.Length > 40){
-                Console.WriteLine("¡Error! Nombre Invalido");
-                Console.WriteLine("Ingrese Nombre");
+
+            while (estudiante.Nombre.Length > 40)
+            {
+                Console.WriteLine("¡Error! Nombre Inválido. Ingrese Nombre (máximo 40 caracteres):");
                 estudiante.Nombre = Console.ReadLine();
             }
-            Console.WriteLine("Ingrese Email");
+
+            Console.WriteLine("Ingrese Email (máximo 40 caracteres):");
             estudiante.Email = Console.ReadLine();
-            while(estudiante.Email.Length > 40){
-                Console.WriteLine("¡Error! Email Invalido");
-                Console.WriteLine("Ingrese Email");
+
+            while (estudiante.Email.Length > 40)
+            {
+                Console.WriteLine("¡Error! Email Inválido. Ingrese Email (máximo 40 caracteres):");
                 estudiante.Email = Console.ReadLine();
             }
-            while(true){
-                try{
-                    Console.WriteLine("Ingrese Edad");
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Ingrese Edad:");
                     estudiante.Edad = int.Parse(Console.ReadLine());
-                    if(estudiante.Edad>100 || estudiante.Edad<=0){
-                        Console.WriteLine("¡Error! Edad Invalida");
+
+                    if (estudiante.Edad > 100 || estudiante.Edad <= 0)
+                    {
+                        Console.WriteLine("¡Error! Edad Inválida.");
                     }
-                    else{
+                    else
+                    {
                         break;
                     }
                 }
-                catch(FormatException){
-                    Console.WriteLine("¡Error! Edad Invalida");
-                }   
+                catch (FormatException)
+                {
+                    Console.WriteLine("¡Error! Edad Inválida.");
+                }
             }
-            listClass.Add(estudiante);
-            Console.Clear();
+
+            return estudiante;
         }
-        
-        Console.WriteLine("{0,-36} {1,-30} {2,-30}", "Codigo", "Nombre", "Email");
-        
-        foreach (Entities estudiante in listClass)
+        static void AgregarNotas()
         {
-            Console.WriteLine("{0,-36} {1,-30} {2,-30}", estudiante.Codigo, estudiante.Nombre, estudiante.Email);
+            
         }
-        
-        // Ahora, puedes usar listClass para trabajar con los estudiantes creados
+
+        static void MostrarEstudiantes(List<Estudiante> estudiantes)
+        {
+            Console.WriteLine("{0,-15} {1,-40} {2,-40} {3,-5}", "Código", "Nombre", "Email", "Edad");
+            
+            foreach (Estudiante estudiante in estudiantes)
+            {
+                Console.WriteLine("{0,-15} {1,-40} {2,-40} {3,-5}", estudiante.Codigo, estudiante.Nombre, estudiante.Email, estudiante.Edad);
+            }
+        }
+    }
+
+    internal class Estudiante
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public int Edad { get; set; }
     }
 }
 
