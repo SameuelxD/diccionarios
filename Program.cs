39e790e698f517ed2c95653e660437911499ee87
Ing.Samuel <126287892+SameuelxD@users.noreply.github.com>
2023-09-09 19:07:59 +0000

    modo guerra papapa

--- Program.cs
+++ Program.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-
 internal class Program
 {
-    static Estudiante estudiante;
-    static Quizz quizz;
     static List<Estudiante> listEstudiantes = new List<Estudiante>();
     static List<Quizz> notasQuizz = new List<Quizz>();
+
     private static void Main(string[] args)
     {
         bool active = true;
@@ -16,10 +14,10 @@ internal class Program
         {
             Console.Clear();
             Console.WriteLine("-------------------- MENU --------------------");
-            Console.WriteLine("   1. Agregar Estudiante. ");
-            Console.WriteLine("   2. Agregar Notas. ");
-            Console.WriteLine("   3. Mostrar Estudiantes. ");
-            Console.WriteLine("   4. Cerrar Programa. ");
+            Console.WriteLine("   1. Agregar Estudiante.");
+            Console.WriteLine("   2. Agregar Notas.");
+            Console.WriteLine("   3. Mostrar Estudiantes.");
+            Console.WriteLine("   4. Cerrar Programa.");
             Console.WriteLine("----------------------------------------------");
             Console.WriteLine("Digite una opcion: ");
             if (int.TryParse(Console.ReadLine(), out int opcion))
@@ -67,47 +65,47 @@ internal class Program
         Console.Clear();
         Console.WriteLine("------------ Agregar Estudiantes -------------");
         Console.WriteLine("----------------------------------------------");
-        estudiante = new Estudiante();
+        Estudiante estudiante = new Estudiante();
 
         Console.WriteLine("Ingrese Código Numérico (máximo 15 caracteres):");
         estudiante.Codigo = Console.ReadLine();
-        estudiante.Codigo=estudiante.Codigo.Replace(" ","");
+        estudiante.Codigo = estudiante.Codigo.Replace(" ", "");
 
         while (estudiante.Codigo.Length > 15 || !int.TryParse(estudiante.Codigo, out int codigo) || codigo <= 0)
         {
             Console.Clear();
             Console.WriteLine("¡Error! Código Inválido. Ingrese Código Numérico (máximo 15 caracteres):");
             estudiante.Codigo = Console.ReadLine();
-            estudiante.Codigo=estudiante.Codigo.Replace(" ","");
+            estudiante.Codigo = estudiante.Codigo.Replace(" ", "");
         }
         Console.Clear();
         Console.WriteLine("Ingrese Nombre (máximo 40 caracteres):");
         estudiante.Nombre = Console.ReadLine();
-        estudiante.Nombre=estudiante.Nombre.Replace(" ","");
-        while(Regex.IsMatch(estudiante.Nombre, @"^\s*$") || !Regex.IsMatch(estudiante.Nombre, @"[a-zA-Z0-9]") || (estudiante.Nombre.Length > 40))
+        estudiante.Nombre = estudiante.Nombre.Replace(" ", "");
+        while (string.IsNullOrWhiteSpace(estudiante.Nombre) || estudiante.Nombre.Length > 40)
         {
             Console.Clear();
-            Console.WriteLine("¡Error! Nombre Inválido. Ingrese Nombre (máximo 40 caracteres):");                    
+            Console.WriteLine("¡Error! Nombre Inválido. Ingrese Nombre (máximo 40 caracteres):");
             estudiante.Nombre = Console.ReadLine();
-            estudiante.Nombre=estudiante.Nombre.Replace(" ","");
+            estudiante.Nombre = estudiante.Nombre.Replace(" ", "");
         }
         Console.Clear();
         Console.WriteLine("Ingrese Email (máximo 40 caracteres):");
         estudiante.Email = Console.ReadLine();
-        estudiante.Email=estudiante.Email.Replace(" ","");
-        while(Regex.IsMatch(estudiante.Email, @"^\s*$") || !Regex.IsMatch(estudiante.Email, @"[a-zA-Z0-9]") || (estudiante.Email.Length > 40))
+        estudiante.Email = estudiante.Email.Replace(" ", "");
+        while (string.IsNullOrWhiteSpace(estudiante.Email) || estudiante.Email.Length > 40 || !IsValidEmail(estudiante.Email))
         {
             Console.Clear();
             Console.WriteLine("¡Error! Email Inválido. Ingrese Email (máximo 40 caracteres):");
             estudiante.Email = Console.ReadLine();
-            estudiante.Email=estudiante.Email.Replace(" ","");
+            estudiante.Email = estudiante.Email.Replace(" ", "");
         }
         Console.Clear();
         while (true)
         {
             try
             {
-                
+
                 Console.WriteLine("Ingrese Edad:");
                 estudiante.Edad = int.Parse(Console.ReadLine());
 
@@ -128,124 +126,130 @@ internal class Program
             }
         }
         Console.Clear();
-        Console.WriteLine("Ingrese Direccion (máximo 35 caracteres):");
+        Console.WriteLine("Ingrese Dirección (máximo 35 caracteres):");
         estudiante.Direccion = Console.ReadLine();
-        estudiante.Direccion=estudiante.Direccion.Replace(" ","");
-        while(Regex.IsMatch(estudiante.Direccion, @"^\s*$") || !Regex.IsMatch(estudiante.Direccion, @"[a-zA-Z0-9]") || (estudiante.Direccion.Length > 35))
+        estudiante.Direccion = estudiante.Direccion.Replace(" ", "");
+        while (string.IsNullOrWhiteSpace(estudiante.Direccion) || estudiante.Direccion.Length > 35)
         {
             Console.Clear();
-            Console.WriteLine("¡Error! Direccion Inválida. Ingrese Direccion (máximo 35 caracteres):");
+            Console.WriteLine("¡Error! Dirección Inválida. Ingrese Dirección (máximo 35 caracteres):");
             estudiante.Direccion = Console.ReadLine();
-            estudiante.Direccion=estudiante.Direccion.Replace(" ","");
-        }  
+            estudiante.Direccion = estudiante.Direccion.Replace(" ", "");
+        }
         return estudiante;
     }
-    static void AgregarNotas()
-    {
-        Console.WriteLine("---------------- Agregar Notas ---------------");
-        Console.WriteLine("   1. Agregar Notas Quizzes.");
-        Console.WriteLine("   2. Agregar Notas Trabajos.");
-        Console.WriteLine("   3. Agregar Notas Parciales.");
-        Console.WriteLine("   4. Volver al Menu Principal.");
-        Console.WriteLine("----------------------------------------------");
-        Console.WriteLine("Digite una opcion: ");
-        if (int.TryParse(Console.ReadLine(), out int opcion))
-        {
-            bool passive=true;
-                while(passive)
-                {
-                    switch (opcion)
-                    {
-                        case 1:
-                            Console.Clear();
-                            Console.WriteLine("1. Agregar Notas Quizzes.");
-                            VerificarEstudiante(listEstudiantes,notasQuizz);
-                            Quizz quizz = AgregarQuizzes();
-                            notasQuizz.Add(quizz);
-                            break;
-                        case 2:
-                            Console.WriteLine("2. Agregar Notas Trabajos.");
-                            break;
-                        case 3:
-                            Console.WriteLine("3. Agregar Notas Parciales.");
-                            MostrarEstudiantes(listEstudiantes);
-                            break;
-                        case 4:
-                            Console.WriteLine("Volviendo al menu principal. Presione una tecla para continuar.");
-                            Console.ReadLine();
-                            passive = false;
-                            break;
-                        default:
-                            Console.WriteLine("Opción no válida. Intente de nuevo.");
-                            break;
-                    }
-                }
-        }
-        else
-        {
-            Console.WriteLine("Entrada no válida. Intente de nuevo.");
-        }
-        
-        
-        static Quizz AgregarQuizzes()
-        {
-            Console.Clear();
-            Console.WriteLine("------------ Agregar Quizzes -------------");
-            Console.WriteLine("----------------------------------------------");
-            quizz = new Quizz();
-            while (true)
-            {
-                try
-                {
-                    
-                    Console.WriteLine("Ingrese Nota Quizz 1:");
-                    quizz.quizz_1 = int.Parse(Console.ReadLine());
-                    
-                    Console.WriteLine("Ingrese Nota Quizz 2:");
-                    quizz.quizz_2 = int.Parse(Console.ReadLine());
-                    
-                    Console.WriteLine("Ingrese Nota Quizz 3:");
-                    quizz.quizz_3 = int.Parse(Console.ReadLine());
-                    
-                    Console.WriteLine("Ingrese Nota Quizz 4:");
-                    quizz.quizz_4 = int.Parse(Console.ReadLine());
 
-                    if ((quizz.quizz_1 > 100 || quizz.quizz_1 < 0) || (quizz.quizz_2 > 100 || quizz.quizz_2 < 0) || (quizz.quizz_3 > 100 || quizz.quizz_3 < 0) || (quizz.quizz_4 > 100 || quizz.quizz_4 < 0))
-                    {
-                        Console.Clear();
-                        Console.WriteLine("¡Error! Nota Quizzes Inválidas.");
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                catch (FormatException)
+    static void AgregarNotas()
+    {   
+        bool passive = true;
+        while (passive)
+        {
+            Console.WriteLine("---------------- Agregar Notas ---------------");
+            Console.WriteLine("   1. Agregar Notas Quizzes.");
+            Console.WriteLine("   2. Agregar Notas Trabajos.");
+            Console.WriteLine("   3. Agregar Notas Parciales.");
+            Console.WriteLine("   4. Volver al Menu Principal.");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Digite una opción: ");
+            if (int.TryParse(Console.ReadLine(), out int opcion))
+            {   
+                switch (opcion)
                 {
-                    Console.Clear();
-                    Console.WriteLine("¡Error! Notas Quizzes Inválidas.");
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("1. Agregar Notas Quizzes.");
+                        string codigoVerificado = VerificarEstudiante(listEstudiantes);
+                        if (codigoVerificado != null)
+                        {
+                            Console.WriteLine($"Estudiante con código {codigoVerificado} verificado correctamente.");
+                            Quizz quizz = AgregarQuizzes();
+                            quizz.Codigo = codigoVerificado;
+                            notasQuizz.Add(quizz);
+                            Console.WriteLine("Notas de Quizz agregadas correctamente.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se encontró ningún estudiante con el código proporcionado.");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("2. Agregar Notas Trabajos.");
+                        break;
+                    case 3:
+                        Console.WriteLine("3. Agregar Notas Parciales.");
+                        MostrarEstudiantes(listEstudiantes);
+                        break;
+                    case 4:
+                        Console.WriteLine("Volviendo al menú principal. Presione una tecla para continuar.");
+                        Console.ReadLine();
+                        passive = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Intente de nuevo.");
+                        break;
                 }
             }
-            return quizz;
+            else
+            {
+                Console.WriteLine("Entrada no válida. Intente de nuevo.");
+            }
         }
     }
-    static string VerificarEstudiante(List<Estudiante> estudiantes, List<Quizz> notasQuizz)
+    static Quizz AgregarQuizzes()
     {
-        Console.WriteLine("Ingrese el Codigo del Estudiante para verificarlo (máximo 15 caracteres): ");
+        Quizz quizz = new Quizz();
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Ingrese Nota Quizz 1:");
+                quizz.Quizz_1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese Nota Quizz 2:");
+                quizz.Quizz_2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese Nota Quizz 3:");
+                quizz.Quizz_3 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese Nota Quizz 4:");
+                quizz.Quizz_4 = int.Parse(Console.ReadLine());
+
+                if ((quizz.Quizz_1 > 100 || quizz.Quizz_1 < 0) || (quizz.Quizz_2 > 100 || quizz.Quizz_2 < 0) || (quizz.Quizz_3 > 100 || quizz.Quizz_3 < 0) || (quizz.Quizz_4 > 100 || quizz.Quizz_4 < 0))
+                {
+                    Console.Clear();
+                    Console.WriteLine("¡Error! Nota Quizzes Inválidas.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.Clear();
+                Console.WriteLine("¡Error! Notas Quizzes Inválidas.");
+            }
+        }
+        return quizz;
+    }
+
+    static string VerificarEstudiante(List<Estudiante> estudiantes)
+    {
+        Console.WriteLine("Ingrese el Código del Estudiante para verificarlo (máximo 15 caracteres): ");
         string codigo = Console.ReadLine();
-        codigo=codigo.Replace(" ","");
+        codigo = codigo.Replace(" ", "");
         while (codigo.Length > 15 || !int.TryParse(codigo, out int code) || code <= 0)
         {
             Console.Clear();
             Console.WriteLine("¡Error! Código Inválido. Ingrese Código del Estudiante para verificarlo (máximo 15 caracteres):");
             codigo = Console.ReadLine();
-            codigo=codigo.Replace(" ","");
+            codigo = codigo.Replace(" ", "");
         }
         foreach (Estudiante estudiante in estudiantes)
         {
-            if(codigo == estudiante.Codigo){
-                Console.WriteLine($"Codigo {codigo} satisfactoriamente encontrado.");
-                return codigo; 
+            if (codigo == estudiante.Codigo)
+            {
+                return codigo;
             }
         }
         return null;
@@ -254,15 +258,29 @@ internal class Program
     static void MostrarEstudiantes(List<Estudiante> estudiantes)
     {
         Console.WriteLine("---------------------------- Mostrar Estudiantes -----------------------------");
-        Console.WriteLine("{0,-15} {1,-35} {2,-35} {3,-35} {4, -5}", "Código", "Nombre", "Email", "Edad", "Direccion");
+        Console.WriteLine("{0,-15} {1,-35} {2,-35} {3,-35} {4, -5}", "Código", "Nombre", "Email", "Edad", "Dirección");
         foreach (Estudiante estudiante in estudiantes)
         {
             Console.WriteLine("{0,-15} {1,-35} {2,-35} {3,-35} {4, -5}", estudiante.Codigo, estudiante.Nombre, estudiante.Email, estudiante.Edad, estudiante.Direccion);
         }
         Console.WriteLine("Presione una tecla para continuar...");
         Console.ReadLine();
-        Console.WriteLine("-----------------------------------------------");    
+        Console.WriteLine("-----------------------------------------------");
     }
+
+    static bool IsValidEmail(string email)
+    {
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(email);
+            return addr.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
     internal class Estudiante
     {
         public string Codigo { get; set; }
@@ -271,6 +289,7 @@ internal class Program
         public int Edad { get; set; }
         public string Direccion { get; set; }
     }
+
     internal class Quizz
     {
         public string Codigo { get; set; }
@@ -280,6 +299,7 @@ internal class Program
         public int Quizz_4 { get; set; }
     }
 }
+                       
 
     
 
